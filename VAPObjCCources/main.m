e15a8a58c5af972e5d30ff1db5d62157e23b99a0
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-07-06 15:02:37 +0300

    Initial Commit

---
+++ VAPObjCCources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VAPObjCCources
+//
+//  Created by Aleksandr Vasylchenko on 06.07.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

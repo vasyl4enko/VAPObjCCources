8cd4dee7fc81942b5eab6213fa65433d23511361
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-07-07 18:29:23 +0300

    VAPHuman+VAPHumanExtension.m -> add extension

---
+++ VAPObjCCources/Source/VAPHuman/VAPHuman+VAPHumanExtension.h
@@ -0,0 +1,15 @@
+//
+//  VAPHuman+VAPHumanExtension.h
+//  VAPObjCCources
+//
+//  Created by Aleksandr Vasylchenko on 07.07.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import "VAPHuman.h"
+
+@interface VAPHuman (VAPHumanExtension)
+
++ (id)object;
+
+@end
---
+++ VAPObjCCources/Source/VAPHuman/VAPHuman+VAPHumanExtension.m
@@ -0,0 +1,17 @@
+//
+//  VAPHuman+VAPHumanExtension.m
+//  VAPObjCCources
+//
+//  Created by Aleksandr Vasylchenko on 07.07.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import "VAPHuman+VAPHumanExtension.h"
+
+@implementation VAPHuman (VAPHumanExtension)
+
++ (id)object{
+    return [[[VAPHuman alloc] init] autorelease];
+}
+
+@end

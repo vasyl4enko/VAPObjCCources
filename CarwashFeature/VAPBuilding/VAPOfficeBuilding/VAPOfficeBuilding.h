1f3ec68d6207c14cc7d35498621655437a0d891d
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-07-13 17:12:42 +0300

    add VAPCarwashBuilding.m VAPOfficeBuilding.m

---
+++ CarwashFeature/VAPBuilding/VAPOfficeBuilding/VAPOfficeBuilding.h
@@ -0,0 +1,13 @@
+//
+//  VAPOfficeBuilding.h
+//  VAPObjCCources
+//
+//  Created by Aleksandr Vasylchenko on 13.07.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import "VAPBuilding.h"
+
+@interface VAPOfficeBuilding : VAPBuilding
+
+@end

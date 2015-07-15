f63cc1eb702fde5a971e9e21165f323b5cdbc911
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-07-15 21:49:42 +0300

    VAPEnterprise.m -> add emty class

---
+++ CarwashFeature/VAPEnterprise/VAPEnterprise.h
@@ -0,0 +1,13 @@
+//
+//  VAPEnterprise.h
+//  VAPObjCCources
+//
+//  Created by Aleksandr Vasylchenko on 15.07.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VAPEnterprise : NSObject
+
+@end
---
+++ CarwashFeature/VAPEnterprise/VAPEnterprise.m
@@ -0,0 +1,13 @@
+//
+//  VAPEnterprise.m
+//  VAPObjCCources
+//
+//  Created by Aleksandr Vasylchenko on 15.07.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import "VAPEnterprise.h"
+
+@implementation VAPEnterprise
+
+@end

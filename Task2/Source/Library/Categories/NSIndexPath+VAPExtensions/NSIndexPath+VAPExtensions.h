45f465edfd504f46d8ffb94f043d305f58026f7c
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-09-25 23:35:32 +0300

    NSIndexPath+VAPExtensions.h -> add category which have + (id)indexPathForRow:(NSUInteger)index;

---
+++ Task2/Source/Library/Categories/NSIndexPath+VAPExtensions/NSIndexPath+VAPExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSIndexPath+VAPExtensions.h
+//  Task2
+//
+//  Created by Aleksandr Vasylchenko on 25.09.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NSIndexPath (VAPExtensions)
+
++ (id)indexPathForRow:(NSUInteger)index;
+
+@end
---
+++ Task2/Source/Library/Categories/NSIndexPath+VAPExtensions/NSIndexPath+VAPExtensions.m
@@ -0,0 +1,19 @@
+//
+//  NSIndexPath+VAPExtensions.m
+//  Task2
+//
+//  Created by Aleksandr Vasylchenko on 25.09.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#import "NSIndexPath+VAPExtensions.h"
+
+static const NSUInteger VAPMagicNumber = 0;
+
+@implementation NSIndexPath (VAPExtensions)
+
++ (id)indexPathForRow:(NSUInteger)index {
+   return [self indexPathForRow:index inSection:VAPMagicNumber];
+}
+
+@end

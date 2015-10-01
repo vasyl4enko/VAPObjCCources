55de512bf87ea1004075dbb52826d678b4069e64
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-10-01 16:32:54 +0300

    small refactoring

--- Task2/Source/Library/Categories/NSString+VAPRandomString/NSString+VAPRandomString.h
+++ Task2/Source/Library/Categories/NSString+VAPRandomString/NSString+VAPRandomString.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
 //
 
-
 #import <Foundation/Foundation.h>
 
 @interface NSString (VAPRandomString)
--- Task2/Source/Models/VAPData/VAPData.h
+++ Task2/Source/Models/VAPData/VAPData.h
@@ -5,6 +5,7 @@
 //  Created by Aleksandr Vasylchenko on 20.09.15.
 //  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
 //
+
 #import <UIKit/UIKit.h>
 
 @interface VAPData : NSObject

3570ad83070b862997ed7363e529e25d251f9492
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-10-30 10:25:44 +0200

    refactoring VAPFileImageModel.m VAPURLImageModel.m

--- Source/Models/VAPURLImageModel/VAPURLImageModel.m
+++ Source/Models/VAPURLImageModel/VAPURLImageModel.m
@@ -1,6 +1,6 @@
 //
 //  VAPURLImageModel.m
-//  
+//
 //
 //  Created by Aleksandr Vasylchenko on 25.10.15.
 //
@@ -18,6 +18,11 @@
 
 @implementation VAPURLImageModel
 
+@dynamic fileFolder;
+@dynamic fileName;
+@dynamic filePath;
+@dynamic cached;
+
 #pragma mark -
 #pragma mark Initializations and Deallocations
 
@@ -26,7 +31,26 @@
 }
 
 #pragma mark -
-#pragma mark Accessors 
+#pragma mark Accessors
+
+- (NSString *)fileFolder {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+}
+
+- (NSString *)fileName {
+    NSString *path = self.imageURL.absoluteString;
+    NSCharacterSet *charactersSet = [NSCharacterSet characterSetWithCharactersInString:@"/."];
+    
+    return [[path componentsSeparatedByCharactersInSet:charactersSet] componentsJoinedByString:@"%20"];
+}
+
+- (NSString *)filePath {
+    return [self.fileFolder stringByAppendingString:self.fileName];
+}
+
+- (BOOL)isCached {
+    return [[NSFileManager defaultManager] fileExistsAtPath:self.filePath];
+}
 
 - (void)setDownloadTask:(NSURLSessionDownloadTask *)downloadTask {
     if (_downloadTask != downloadTask) {
@@ -57,7 +81,7 @@
 
 - (void)performLoadingWithCompletion:(void(^)(UIImage *image, id error))completion {
     if (self.cached) {
-        [super performLoadingWithCompletion:completion];
+        [self loadImageWithFilePath:completion];
     } else {
         [self downloadImage:completion];
     }
@@ -71,10 +95,27 @@
         [[NSFileManager defaultManager] copyItemAtURL:url
                                                 toURL:[NSURL fileURLWithPath:self.filePath]
                                                 error:nil];
-        [super performLoadingWithCompletion:completion];
+        [self loadImageWithFilePath:completion];
     };
     
     self.downloadTask = [self.urlSession downloadTaskWithURL:self.imageURL completionHandler:block];
+}
+
+- (void)removeFromCache {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error = nil;
+    [fileManager removeItemAtPath:self.filePath error:&error];
+}
+
+- (void)loadImageWithFilePath:(void (^)(UIImage *, id))completion {
+    UIImage *image = [UIImage imageWithContentsOfFile:self.filePath];
+    if (!image) {
+        [self removeFromCache];
+    }
+    
+    if(completion) {
+        completion(image, nil);
+    }
 }
 
 @end
